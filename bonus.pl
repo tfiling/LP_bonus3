0c61f868dbb97e7b45d2448087b27617731cd470
gal tfilin <tfiling@post.bgu.ac.il>
2018-06-09 22:23:35 +0300

    implemented BEE compatible addition and multiplication operators

--- bonus.pl
+++ bonus.pl
@@ -4,6 +4,7 @@
 
 % TODO ask mike:
 % on verify Im assuming the input a list of digits and just count the appearances
+% which base should I use for accuracy with large numbers? it s
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% part 1: verify
@@ -77,7 +78,9 @@ calculateIthTopSumComponent(I, NumeratorsList, DenominatorsList, Component) :-
 %% crt representation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-base([2, 3, 5, 7, 11]).
+base([2, 3, 5, 7, 11, 13, 17, 19]).
+% prime numbers
+% 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
 
 crtRepresentation(Num, Repr) :-
     base(Base),
@@ -124,6 +127,54 @@ crtMultiplyAll([X1, X2 | Rest], Result) :-
     crtMultiplication(X1, X2, Res),
     crtMultiplyAll([Res | Rest], Result).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% BEE compatible crt
+
+beeCrtAddition(Num1, Num2, Result, Cs-Tail) :-
+    base(Base),
+    beeCrtAddition(Base, Num1, Num2, Result-[], Cs-Tail).
+
+beeCrtAddition([], [], [], Tail-Tail, CsTail-CsTail).
+beeCrtAddition([HBase | RestBase], [H1 | Rest1], [H2 | Rest2], [Res | RestRes]-Tail, Cs-CsTail) :-
+    HBase2 is HBase * 2,
+    % Sum is H1 + H2,
+    % Res is mod(Sum, HBase),
+    Cs = [
+        new_int(Sum, 0, HBase2), 
+        int_plus(H1, H2, Sum),
+        new_int(Res, 0, HBase),
+        int_mod(Sum, HBase, Res) | RestCs ],
+    beeCrtAddition(RestBase, Rest1, Rest2, RestRes-Tail, RestCs-CsTail).
+
+
+% beeCrtSumAll(NumList+, Result-, Cs-) - aggregative add all crt numbers in NumList into the sum Result
+beeCrtSumAll([Result], Result, Tail-Tail).
+beeCrtSumAll([X1, X2 | Rest], Result, Cs-Tail) :-
+    beeCrtAddition(X1, X2, Res, Cs-Cs2),
+    beeCrtSumAll([Res | Rest], Result, Cs2-Tail).
+
+
+beeCrtMultiplication(Num1, Num2, Result, Cs-Tail) :-
+    base(Base),
+    beeCrtMultiplication(Base, Num1, Num2, Result-[], Cs-Tail).
+
+beeCrtMultiplication([], [], [], Tail-Tail, CsTail-CsTail).
+beeCrtMultiplication([HBase | RestBase], [H1 | Rest1], [H2 | Rest2], [Res | RestRes]-Tail, Cs-CsTail) :-
+    HBase2 is HBase * HBase,
+    % Mul is H1 * H2,
+    % Res is mod(Mul, HBase),
+    Cs = [
+        new_int(MulRes, 0, HBase2),
+        int_mul(H1, H2, MulRes),
+        new_int(Res, 0, HBase),
+        int_mod(MulRes, HBase, Res) | RestCs ],
+    beeCrtMultiplication(RestBase, Rest1, Rest2, RestRes-Tail, RestCs-CsTail).
+
+% beeCrtMultiplyAll(NumList+, Result-, Cs-) - aggregative multiply all crt numbers in NumList
+beeCrtMultiplyAll([Result], Result, Tail-Tail).
+beeCrtMultiplyAll([X1, X2 | Rest], Result, Cs-Tail) :-
+    beeCrtMultiplication(X1, X2, Res, Cs-Cs2),
+    beeCrtMultiplyAll([Res | Rest], Result, Cs2-Tail).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
